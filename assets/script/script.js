8ed860aac6ae7571da342fce751c004838a337c1
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-19 15:04:32 +0000

    [fix] style

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,15 +11,16 @@ createApp({
   methods: {
     getApi() {
       for (let i = 0; i < 10; i++) {
-        axios.get(this.apiUrl).then((res) => {
-          console.log(res.data);
-          this.emailList.push(res.data.response);
-        })
-        .catch( (err) => {
-          console.log(err);
-        })
+        axios
+          .get(this.apiUrl)
+          .then((res) => {
+            console.log(res.data);
+            this.emailList.push(res.data.response);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
-      console.log(this.emailList);
     },
   },
   mounted() {
