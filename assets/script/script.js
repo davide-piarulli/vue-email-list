3411ba11943eb6cfe70d63ae8a26b6aefeeb3f27
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-19 14:34:27 +0000

    [add] log 10 email

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,29 @@
 const {createApp} = Vue;
 
+createApp({
+  data(){
+    return {
+      title: '',
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emailList : [],
+    }
+  },
+  methods: {
+    getApi(){
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get(this.apiUrl)
+          .then((response) => {
+          console.log(response.data);
+        })
+
+        
+      }
+
+    }
+
+  },
+  mounted(){
+    this.getApi();
+  },
+}).mount('#app')
